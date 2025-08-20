edbdaf4e4e640bdf205fa4c1a77ef37c274f43dd
Thubelihle Kyle Ndlovu <tkndlovu111@gmail.com>
2025-08-20 14:35:25 +0200

    First version of my analysis in SQL

---
+++ bright_coffee_analysis_script.sql
@@ -0,0 +1,50 @@
+SELECT*,
+
+---------Data concerption
+
+--------Dates
+
+TO_DATE(transaction_date)AS transaction_date,
+DAYOFMONTH (TO_DATE(transaction_date)) AS day_of_month,
+DAYNAME (TO_DATE(transaction_date)) AS  day_name,
+MONTHNAME (TO_DATE(transaction_date)) AS  month_name,
+
+CASE
+    WHEN transaction_time BETWEEN '06:00:00' AND '11:59:59' THEN 'Morning'
+    WHEN transaction_time BETWEEN '12:00:00' AND '15:59:59' THEN 'Afternoon'
+    WHEN transaction_time BETWEEN '16:00:00' AND '18:59:59' THEN 'Evening'
+    ELSE 'Night'
+    END AS time_bucket,
+
+CASE 
+    WHEN DAYNAME(TO_DATE(transaction_date)) NOT IN ('Sat','Sun') THEN 'Morning'
+    ELSE 'Weekend'
+    END AS day_type,
+
+CASE 
+    WHEN unit_price < 10 THEN 'Low Price'
+    WHEN unit_price BETWEEN 10 AND 25 THEN 'Medium Price'
+    ELSE 'High Price'
+    END AS price_bucket,
+    
+ROUND(SUM(transaction_qty * unit_price)) AS total_revenue,
+
+-----------COUNT     
+
+COUNT(DISTINCT transaction_id)AS number_of_sales,
+COUNT(DISTINCT product_id)AS unique_products,
+COUNT(DISTINCT store_id)AS unique_stores,
+
+------Categories
+
+product_category,
+product_type,
+product_detail,
+store_location
+
+
+
+FROM BRIGHTCOFFEE_SHOP.SALES_DATA.STORES
+
+GROUP BY ALL;
+
